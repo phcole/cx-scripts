1b1e3d639e92f6d1f5268c7f575483af33273ae7
Hao Peng <penghao@linuxdeepin.com>
2016-01-28 17:09:02 +0800

    modify version numbers.
    Signed-off-by: Hao Peng <penghao@linuxdeepin.com>

--- package-richez.sh
+++ package-richez.sh
@@ -5,7 +5,7 @@ export public_bottle_name="Deepin-RichEz"
 export public_bottle_uuid="828c0ec4-4829-4874-a747-a93776a677d7"
 export desktop_file_categories="others;"
 export deb_package_name="deepinwine-richez"
-export deb_version_string="1.10deepin1"
+export deb_version_string="1.19deepin0"
 export deb_description="GUOTAI JUNAN SECURITIES RichEz Client on CrossOver Deepin"
 
 ./script-packager.sh
--- package-rtx.sh
+++ package-rtx.sh
@@ -5,7 +5,7 @@ export public_bottle_name="Deepin-RTX"
 export public_bottle_uuid="0bc13f75-c806-4eb0-8013-340326ddf067"
 export desktop_file_categories="chat;"
 export deb_package_name="deepinwine-rtx"
-export deb_version_string="8.3.625.1deepin1"
+export deb_version_string="8.3.625.1deepin4"
 export deb_description="Tencent RTX 2013 Client on CrossOver Deepin"
 
 ./script-packager.sh
