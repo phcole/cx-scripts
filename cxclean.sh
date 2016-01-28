6ce152c09848542b65dd0239a6a88ce118d971d5
Hao Peng <penghao@linuxdeepin.com>
2016-01-28 17:07:48 +0800

    update cxcleaner.sh.
    Signed-off-by: Hao Peng <penghao@linuxdeepin.com>

--- cxclean.sh
+++ cxclean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WINE="/opt/cxoffice/bin/wine"
+WINE_CMD="/opt/cxoffice/bin/wine"
 
 remove_file()
 {
@@ -21,17 +21,17 @@ purge_dir()
 shutdown_bottle()
 {
     echo "==>Shutdown Original Bottle..."
-    $WINE --bottle="$BottleName" --no-update --wl-app wineboot.exe -- --end-session --shutdown --force --kill
+    $WINE_CMD --bottle="$BottleName" --no-update --wl-app wineboot.exe -- --end-session --shutdown --force --kill
 
     # i don't know why 1 for not running, 0 for running....
-    while [ "$?" -eq "0" ]; do $WINE --bottle="$BottleName" --no-update --ux-app wineserver -k0;done
+    while [ "$?" -eq "0" ]; do $WINE_CMD --bottle="$BottleName" --no-update --ux-app wineserver -k0;done
     echo "<==done."
 }
 
 clean_gecko()
 {
     if [ ! -e "bin/gecko-cleaner" ]; then
-        echo "gecko cleaner found, skip."
+        echo "gecko cleaner not found, skip."
         return
     fi
 
@@ -40,14 +40,17 @@ clean_gecko()
 #    /opt/cxoffice/bin/wine --bottle="$BottleName" reg delete "HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall\{43E91AE4-1065-4255-B035-3035D5287CF8}" # 2.40
 #    # remove all old gecko kernels
 #    for i in $(ls "$BottleBase/windows/system32/gecko/" | grep -v plugin | sort -V |head -n -1);do
-    for i in $(ls "$BottleBase/windows/system32/gecko/" | grep -v plugin);do
-        echo "=============remove gecko dir [$i]"
-        rm -rfv "$BottleBase/windows/system32/gecko/$i"
-    done
+    if [ -e "$BottleBase/windows/system32/gecko" ];then
+        for i in $(ls "$BottleBase/windows/system32/gecko/" | grep -v plugin);do
+            echo "=============remove gecko dir [$i]"
+            rm -rfv "$BottleBase/windows/system32/gecko/$i"
+        done
+    fi
 }
 
 clean_temp()
 {
+    remove_file "$BottleBase/../winetricks.log"
     purge_dir   "$BottleBase/windows/temp"
     purge_dir   "$BottleBase/windows/Installer"
     purge_dir   "$BottleBase/users/crossover/Temp"
@@ -58,6 +61,80 @@ clean_temp()
     purge_dir   "$BottleBase/users/crossover/Application Data/wine_gecko"
     purge_dir   "$BottleBase/users/crossover/Local Settings/Temporary Internet Files"
     remove_file "$BottleBase/users/phcole"
+}
+
+cleanup_aliwangwang()
+{
+    if [ ! -e "$BottleBase/Program Files/AliWangWang/AliIM.exe" ];then return;fi
+
+    echo "++++AliWangWang detected. cleaning..."
+    purge_dir   "$BottleBase/Program Files/AliWangWang/profiles"
+    purge_dir   "$BottleBase/Program Files/AliWangWang/new"
+}
+
+cleanup_aliworkbench()
+{
+    if [ ! -e "$BottleBase/Program Files/AliWorkbench/AliWorkbench.exe" ];then return;fi
+
+    echo "++++AliWorkbench detected. cleaning..."
+    purge_dir   "$BottleBase/users/crossover/Local Settings/Application Data/aef/"
+    purge_dir   "$BottleBase/users/Public/Documents/AliWorkbench/"
+}
+
+cleanup_flash()
+{
+    if [ ! -e "$BottleBase/windows/system32/Macromed/Flash/" ];then return;fi
+
+    echo "++++flash detected. cleaning..."
+    remove_file "$BottleBase/users/crossover/Application Data/Adobe"
+    remove_file "$BottleBase/users/crossover/Application Data/Macromedia"
+    remove_file "$BottleBase/windows/system32/FlashPlayerApp.exe"
+    remove_file "$BottleBase/windows/ControlPanelDB/FlashPlayerCPLApp.ico"
+    remove_file "$BottleBase/windows/system32/FlashPlayerCPLApp.cpl"
+    remove_file "$BottleBase/windows/system32/Macromed/Flash/FlashInstall.log"
+    #remove_file "$BottleBase/windows/system32/Macromed/Flash/Flash*.dll"
+    #remove_file "$BottleBase/windows/system32/Macromed/Flash/Flash*.exe"
+    #remove_file "$BottleBase/windows/system32/Macromed/Flash/mms.cfg"
+}
+
+cleanup_foobar2000()
+{
+    if [ ! -e "$BottleBase/Program Files/foobar2000/foobar2000.exe" ];then return;fi
+
+    echo "++++foobar2000 detected. cleaning..."
+    purge_dir   "$BottleBase/Program Files/foobar2000/configuration"
+    purge_dir   "$BottleBase/Program Files/foobar2000/library"
+    purge_dir   "$BottleBase/Program Files/foobar2000/playlists-v1.3"
+    remove_file "$BottleBase/Program Files/foobar2000/theme.fth"
+}
+
+cleanup_mailmaster()
+{
+    if [ ! -e "$BottleBase/MailMaster/MailMaster.exe" ];then return;fi
+
+    echo "++++MailMaster detected. cleaning..."
+    remove_file "$BottleBase/MailMaster/account"
+    remove_file "$BottleBase/MailMaster/address"
+    remove_file "$BottleBase/MailMaster/tmp"
+    remove_file "$BottleBase/MailMaster/UpdateTemp"
+    remove_file "$BottleBase/MailMaster/applog.txt"
+    remove_file "$BottleBase/MailMaster/netlog.txt"
+    remove_file "$BottleBase/MailMaster/global.cfg"
+    remove_file "$BottleBase/MailMaster/schd.sdb"
+}
+
+cleanup_nativeie()
+{
+    if [ ! -e "$BottleBase/Program Files/Internet Explorer/iexplore.exe.mui" ];then return;fi
+
+    echo "++++native IE detected. cleaning..."
+    purge_dir   "$BottleBase/users/crossover/Local Settings/Application Data/Microsoft/"
+
+    remove_file "$BottleBase/windows/ie8"
+    remove_file "$BottleBase/windows/%DownloadedProgramFiles%"
+
+    #remove all useless gecko files
+    remove_file "$BottleBase/windows/system32/gecko"
 }
 
 cleanup_qq()
@@ -101,70 +178,25 @@ cleanup_qqdownload()
     purge_dir   "$BottleBase/users/crossover/Application Data/Tencent/QQDownload"
 }
 
-cleanup_foobar2000()
-{
-    if [ ! -e "$BottleBase/Program Files/foobar2000/foobar2000.exe" ];then return;fi
-
-    echo "++++foobar2000 detected. cleaning..."
-    purge_dir   "$BottleBase/Program Files/foobar2000/configuration"
-    purge_dir   "$BottleBase/Program Files/foobar2000/library"
-    purge_dir   "$BottleBase/Program Files/foobar2000/playlists-v1.3"
-    remove_file "$BottleBase/Program Files/foobar2000/theme.fth"
-}
-
-cleanup_aliwangwang()
-{
-    if [ ! -e "$BottleBase/Program Files/AliWangWang/AliIM.exe" ];then return;fi
-
-    echo "++++AliWangWang detected. cleaning..."
-    purge_dir   "$BottleBase/Program Files/AliWangWang/profiles"
-    purge_dir   "$BottleBase/Program Files/AliWangWang/new"
-}
-
-
-cleanup_flash()
-{
-    echo "++++flash detected. cleaning..."
-    remove_file "$BottleBase/users/crossover/Application Data/Adobe"
-    remove_file "$BottleBase/users/crossover/Application Data/Macromedia"
-    remove_file "$BottleBase/windows/system32/FlashPlayerApp.exe"
-    remove_file "$BottleBase/windows/ControlPanelDB/FlashPlayerCPLApp.ico"
-    remove_file "$BottleBase/windows/system32/FlashPlayerCPLApp.cpl"
-    remove_file "$BottleBase/windows/system32/Macromed/Flash/FlashInstall.log"
-    remove_file "$BottleBase/windows/system32/Macromed/Flash/Flash*.dll"
-    remove_file "$BottleBase/windows/system32/Macromed/Flash/Flash*.exe"
-    remove_file "$BottleBase/windows/system32/Macromed/Flash/mms.cfg"
-}
-
-cleanup_mailmaster()
-{
-    if [ ! -e "$BottleBase/MailMaster/MailMaster.exe" ];then return;fi
-
-    echo "++++MailMaster detected. cleaning..."
-    remove_file "$BottleBase/MailMaster/account"
-    remove_file "$BottleBase/MailMaster/address"
-    remove_file "$BottleBase/MailMaster/tmp"
-    remove_file "$BottleBase/MailMaster/UpdateTemp"
-    remove_file "$BottleBase/MailMaster/applog.txt"
-    remove_file "$BottleBase/MailMaster/netlog.txt"
-    remove_file "$BottleBase/MailMaster/global.cfg"
-    remove_file "$BottleBase/MailMaster/schd.sdb"
-}
-
-
 software_cleaner()
 {
+    cleanup_aliwangwang
+    cleanup_aliworkbench
+    cleanup_flash
+    cleanup_foobar2000
+    cleanup_mailmaster
+    cleanup_nativeie
     cleanup_qq
     cleanup_qqdownload
-    cleanup_foobar2000
-    cleanup_aliwangwang
-    cleanup_mailmaster
-    #cleanup_flash
 }
 
 optimize()
 {
+    echo "++++optimizing & tweaking..."
+    $WINE_CMD --bottle="$BottleName" -- reg DELETE "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Applets\\Regedit" /v "LastKey" /f 2>&-
     sed -i 's#\(C:\\\\windows\\\\system32\\\\winebrowser\(\.exe\)\?\) #\\\"\1\\\" #' "$BottleBase/../system.reg"
+    sed -i "s#$USER#crossover#" $BottleBase/../*.reg
+
 }
 
 #=============================================
@@ -190,7 +222,7 @@ fi
 clean_gecko
 clean_temp
 software_cleaner
-#optimize
+optimize
 shutdown_bottle
 
 exit 0
