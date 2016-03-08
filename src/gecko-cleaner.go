7e3f473c8b478c9a46672a2e5d53d2cb5208f4bc
Peng <penghao@linuxdeepin.com>
2016-03-08 10:24:52 +0800

    Output error when failed to call uninstaller.
    Signed-off-by: Peng <penghao@linuxdeepin.com>

--- src/gecko-cleaner.go
+++ src/gecko-cleaner.go
@@ -2,6 +2,7 @@ package main
 
 import ("os";"os/exec";"flag";"fmt";"strings")
 
+const WineUninstallerPath = "c:/windows/system32/uninstaller.exe"
 const WeWantToRemove = "Wine Gecko (32-bit)"
 
 var bottle string
@@ -17,7 +18,8 @@ func main() {
         flag. PrintDefaults()
         os.Exit(1)
     }
-    out, err := exec.Command("/opt/cxoffice/bin/wine", "--bottle=" + bottle, "uninstaller", "--list").Output()
+    fmt.Printf("=>> Start to uninstall gecko...\n")
+    out, err := exec.Command("/opt/cxoffice/bin/wine", "--bottle=" + bottle, "--", WineUninstallerPath, "--list").Output()
     if err != nil {
         fmt.Printf("ERR %s\n", err.Error())
     }
@@ -26,12 +28,15 @@ func main() {
     for _, v := range arr {
         if len(v) == 0 { continue }
         s := strings.Split(v, "|||")
-        if s[1] == WeWantToRemove {
-            fmt.Printf("==>uninstall %s:%s...\n", s[1], s[0])
-            exec.Command("/opt/cxoffice/bin/wine", "--bottle=" + bottle, "uninstaller", "--remove", s[0]).Run()
+        if strings.HasPrefix(s[1], WeWantToRemove) {
+            err := exec.Command("/opt/cxoffice/bin/wine", "--bottle=" + bottle, "--", WineUninstallerPath, "--remove", s[0]).Run()
+            if err != nil {
+                fmt.Printf("ERR %s\n", err.Error())
+            } else {
+                counts++
+            }
             fmt.Printf("<==done.\n")
-            counts+=1
         }
     }
-    fmt.Printf("%d uninstalled.\n", counts)
+    fmt.Printf("<<= %d uninstalled.\n", counts)
 }
